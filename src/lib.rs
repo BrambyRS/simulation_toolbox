3b2bb9d39872aaac8cf7fb8ab1d1143a7dd8774a
Ranbir Singh <ranbir1998@gmail.com>
2025-10-25 00:52:20 +0100

    Add Model trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,9 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub trait Model {
+    fn name(&self) -> &str;
+    fn n_x(&self) -> usize;
+    fn n_u(&self) -> usize;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    fn f(&self, x: &Vec<f64>, u: &Vec<f64>, t: f64) -> Vec<f64>; // Dynamics function
+    fn j(&self, x: &Vec<f64>, u: &Vec<f64>, t: f64) -> Vec<f64>; // Jacobian of f w.r.t x, u, t
 }
