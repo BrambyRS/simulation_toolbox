4fef1c1b0e46e2feb1903a7e8a3eb26882f7a9d6
Ranbir Singh <ranbir1998@gmail.com>
2025-10-28 23:03:28 +0000

    Rename f and j to longer names

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,6 @@ pub trait Model {
     fn n_x(&self) -> usize;
     fn n_u(&self) -> usize;
 
-    fn f(&self, x: &Vec<f64>, u: &Vec<f64>, t: f64) -> Vec<f64>; // Dynamics function
-    fn j(&self, x: &Vec<f64>, u: &Vec<f64>, t: f64) -> Vec<f64>; // Jacobian of f w.r.t x, u, t
+    fn fun(&self, x: &Vec<f64>, u: &Vec<f64>, t: f64) -> Vec<f64>; // Dynamics function
+    fn jac(&self, x: &Vec<f64>, u: &Vec<f64>, t: f64) -> Vec<f64>; // Jacobian of f w.r.t x, u, t
 }
